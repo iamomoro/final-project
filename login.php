24975f8f283da704e48795a3f27404021c064139
Elvis Omoro <iamomoroit@gmail.com>
2023-01-11 04:06:16 +0300

    remove credits

--- login.php
+++ login.php
@@ -1,30 +1,26 @@
-<?php 
+<?php
 session_start();
 include("config.php");
-$error="";
-$msg="";
-if(isset($_REQUEST['login']))
-{
-	$email=$_REQUEST['email'];
-	$pass=$_REQUEST['pass'];
-	$pass= sha1($pass);
-	
-	if(!empty($email) && !empty($pass))
-	{
+$error = "";
+$msg = "";
+if (isset($_REQUEST['login'])) {
+	$email = $_REQUEST['email'];
+	$pass = $_REQUEST['pass'];
+	$pass = sha1($pass);
+
+	if (!empty($email) && !empty($pass)) {
 		$sql = "SELECT * FROM user where uemail='$email' && upass='$pass'";
-		$result=mysqli_query($con, $sql);
-		$row=mysqli_fetch_array($result);
-		   if($row){
-			   
-				$_SESSION['uid']=$row['uid'];
-				$_SESSION['uemail']=$email;
-				header("location:index.php");
-				
-		   }
-		   else{
-			   $error = "<p class='alert alert-warning'>Email or Password doesnot match!</p> ";
-		   }
-	}else{
+		$result = mysqli_query($con, $sql);
+		$row = mysqli_fetch_array($result);
+		if ($row) {
+
+			$_SESSION['uid'] = $row['uid'];
+			$_SESSION['uemail'] = $email;
+			header("location:index.php");
+		} else {
+			$error = "<p class='alert alert-warning'>Email or Password doesnot match!</p> ";
+		}
+	} else {
 		$error = "<p class='alert alert-warning'>Please Fill all the fields</p>";
 	}
 }
@@ -33,40 +29,41 @@ if(isset($_REQUEST['login']))
 <html lang="en">
 
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Meta Tags -->
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<link rel="shortcut icon" href="images/favicon.ico">
+	<!-- Meta Tags -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="shortcut icon" href="images/favicon.ico">
 
-<!--	Fonts
+	<!--	Fonts
 	========================================================-->
-<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
 
-<!--	Css Link
+	<!--	Css Link
 	========================================================-->
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
-<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
-<link rel="stylesheet" type="text/css" href="css/layerslider.css">
-<link rel="stylesheet" type="text/css" href="css/color.css">
-<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
-<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" type="text/css" href="css/login.css">
-<!-- FOR MORE PROJECTS visit: codeastro.com -->
-<!--	Title
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+	<link rel="stylesheet" type="text/css" href="css/color.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<!-- FOR MORE PROJECTS visit: .com -->
+	<!--	Title
 	=========================================================-->
-<title>Real Estate PHP</title>
+	<title>Real Estate PHP</title>
 </head>
+
 <body>
 
-<!--	Page Loader
+	<!--	Page Loader
 =============================================================
 <div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
 	<div class="d-flex justify-content-center y-middle position-relative">
@@ -75,17 +72,17 @@ if(isset($_REQUEST['login']))
 	  </div>
 	</div>
 </div>
---> 
+-->
 
 
-<div id="page-wrapper">
-    <div class="row"> 
-        <!--	Header start  -->
-		<?php include("include/header.php");?>
-        <!--	Header end  -->
-        
-        <!--	Banner   --->
-        <!-- <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+	<div id="page-wrapper">
+		<div class="row">
+			<!--	Header start  -->
+			<?php include("include/header.php"); ?>
+			<!--	Header end  -->
+
+			<!--	Banner   --->
+			<!-- <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
@@ -102,85 +99,86 @@ if(isset($_REQUEST['login']))
                 </div>
             </div>
         </div> -->
-         <!--	Banner   --->
-		 
-		 
-		 
-        <div class="page-wrappers login-body full-row bg-gray">
-            <div class="login-wrapper">
-            	<div class="container">
-                	<div class="loginbox">
-                        <div class="login-right">
-							<div class="login-right-wrap">
-								<h1>Login</h1>
-								<p class="account-subtitle">Access to our dashboard</p>
-								<?php echo $error; ?><?php echo $msg; ?>
-								<!-- Form -->
-								<form method="post">
-									<div class="form-group">
-										<input type="email"  name="email" class="form-control" placeholder="Your Email*">
-									</div>
-									<div class="form-group">
-										<input type="password" name="pass"  class="form-control" placeholder="Your Password">
-									</div>
-									
+			<!--	Banner   --->
+
+
+
+			<div class="page-wrappers login-body full-row bg-gray">
+				<div class="login-wrapper">
+					<div class="container">
+						<div class="loginbox">
+							<div class="login-right">
+								<div class="login-right-wrap">
+									<h1>Login</h1>
+									<p class="account-subtitle">Access to our dashboard</p>
+									<?php echo $error; ?><?php echo $msg; ?>
+									<!-- Form -->
+									<form method="post">
+										<div class="form-group">
+											<input type="email" name="email" class="form-control" placeholder="Your Email*">
+										</div>
+										<div class="form-group">
+											<input type="password" name="pass" class="form-control" placeholder="Your Password">
+										</div>
+
 										<button class="btn btn-success" name="login" value="Login" type="submit">Login</button>
-									
-								</form>
-								
-								<div class="login-or">
-									<span class="or-line"></span>
-									<span class="span-or">or</span>
-								</div>
-								<!-- FOR MORE PROJECTS visit: codeastro.com -->
-								<!-- Social Login -->
-								<!-- <div class="social-login">
+
+									</form>
+
+									<div class="login-or">
+										<span class="or-line"></span>
+										<span class="span-or">or</span>
+									</div>
+									<!-- FOR MORE PROJECTS visit: .com -->
+									<!-- Social Login -->
+									<!-- <div class="social-login">
 									<span>Login with</span>
 									<a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
 									<a href="#" class="google"><i class="fab fa-google"></i></a>
 									<a href="#" class="facebook"><i class="fab fa-twitter"></i></a>
 									<a href="#" class="google"><i class="fab fa-instagram"></i></a>
 								</div> -->
-								<!-- /Social Login -->
-								
-								<div class="text-center dont-have">Don't have an account? <a href="register.php">Register</a></div>
-								
-							</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-	<!--	login  -->
-        
-        
-        <!--	Footer   start--><!-- FOR MORE PROJECTS visit: codeastro.com -->
-		<?php include("include/footer.php");?>
-		<!--	Footer   start-->
-        
-        <!-- Scroll to top --> 
-        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
-        <!-- End Scroll To top --> 
-    </div>
-</div>
-<!-- Wrapper End --> 
+									<!-- /Social Login -->
 
-<!--	Js Link
-============================================================--> 
-<script src="js/jquery.min.js"></script> 
-<!--jQuery Layer Slider --> 
-<script src="js/greensock.js"></script> 
-<script src="js/layerslider.transitions.js"></script> 
-<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
-<!--jQuery Layer Slider --> 
-<script src="js/popper.min.js"></script> 
-<script src="js/bootstrap.min.js"></script> 
-<script src="js/owl.carousel.min.js"></script> 
-<script src="js/tmpl.js"></script> 
-<script src="js/jquery.dependClass-0.1.js"></script> 
-<script src="js/draggable-0.1.js"></script> 
-<script src="js/jquery.slider.js"></script> 
-<script src="js/wow.js"></script> 
-<script src="js/custom.js"></script>
+									<div class="text-center dont-have">Don't have an account? <a href="register.php">Register</a></div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--	login  -->
+
+
+			<!--	Footer   start--><!-- FOR MORE PROJECTS visit: .com -->
+			<?php include("include/footer.php"); ?>
+			<!--	Footer   start-->
+
+			<!-- Scroll to top -->
+			<a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a>
+			<!-- End Scroll To top -->
+		</div>
+	</div>
+	<!-- Wrapper End -->
+
+	<!--	Js Link
+============================================================-->
+	<script src="js/jquery.min.js"></script>
+	<!--jQuery Layer Slider -->
+	<script src="js/greensock.js"></script>
+	<script src="js/layerslider.transitions.js"></script>
+	<script src="js/layerslider.kreaturamedia.jquery.js"></script>
+	<!--jQuery Layer Slider -->
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/tmpl.js"></script>
+	<script src="js/jquery.dependClass-0.1.js"></script>
+	<script src="js/draggable-0.1.js"></script>
+	<script src="js/jquery.slider.js"></script>
+	<script src="js/wow.js"></script>
+	<script src="js/custom.js"></script>
 </body>
+
 </html>
